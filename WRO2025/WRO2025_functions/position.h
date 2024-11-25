fa639cffc8df8fca093920ce5cb2e3305aafb02d
Ian Parizot <parizot.ian@gmail.com>
2024-11-25 17:15:56 +0000

    Création de signal.h et mise à jour de position.h

--- WRO2025/WRO2025_functions/position.h
+++ WRO2025/WRO2025_functions/position.h
@@ -1,12 +1,15 @@
 #ifndef POSITION_H
 #define POSITION_H
 #include <time.h>
+#include "signal.h"
+
+int a;
 
 double time_to_collision_at_rest(float velocity) {
     clock_t start = clock();
     do {
-        velocity++;
-    } while (velocity < 1000000);
+        a = signal();
+    } while (a == 1);
     clock_t end = clock();
     double time_to_reception = ((double)(end-start)) / CLOCKS_PER_SEC;
     return time_to_reception;
---
+++ WRO2025/WRO2025_functions/signal.h
@@ -0,0 +1,16 @@
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+inline int signal() {
+    int trueornot;
+    if (trueornot == 0)
+    {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+    
+}
+
+#endif SIGNAL_H
